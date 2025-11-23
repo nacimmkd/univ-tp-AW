8daa59c1fd45ff15b3c196d4a5c52a6410aae817
nacimmkd <nacimmakedhi@gmail.com>
2025-11-23 04:17:47 +0100

    finished with qustion 8

--- tp6/meteo-angular/src/app/app.module.ts
+++ tp6/meteo-angular/src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+// debut du fichier
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { DatePipe } from "@angular/common";
+import { RouterModule , Routes } from '@angular/router'; 
+import { AppRoutingModule } from './app-routing.module'; 
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// <-- dans la suite du TP, Ajouter les références aux autres modules ici aussi
+
+import { AppComponent } from "./app.component";
+import { MeteoComponent } from "./meteo/meteo.component";
+// <-- dans la suite du TP, Ajouter les références à MeteoDetailComponent aussi
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeteoComponent,
+    // <-- dans la suite du TP,ajouter MeteoDetailComponent ici
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,      
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [DatePipe],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
