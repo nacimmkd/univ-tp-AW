48173d52c43d4613853e4ea7198568412fbb9e80
nacimmkd <nacimmakedhi@gmail.com>
2025-11-23 05:04:14 +0100

    v0.1 : first version of app

--- tp6/meteo-angular/src/app/app-routing.module.ts
+++ tp6/meteo-angular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MeteoDetailComponent } from './meteo-detail/meteo-detail.component';
+import { MeteoComponent } from './meteo/meteo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'meteo/:name', component: MeteoDetailComponent },
+  { path: '', component: MeteoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- tp6/meteo-angular/src/app/app.module.ts
+++ tp6/meteo-angular/src/app/app.module.ts
@@ -5,22 +5,41 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { DatePipe } from "@angular/common";
 import { RouterModule , Routes } from '@angular/router'; 
 import { AppRoutingModule } from './app-routing.module'; 
+import { CommonModule } from "@angular/common";
 
-// <-- dans la suite du TP, Ajouter les références aux autres modules ici aussi
 
 import { AppComponent } from "./app.component";
 import { MeteoComponent } from "./meteo/meteo.component";
+import { MeteoDetailComponent } from "./meteo-detail/meteo-detail.component";
 // <-- dans la suite du TP, Ajouter les références à MeteoDetailComponent aussi
+
+const appRoutes: Routes = [
+  {
+    path: "", // la page principale utilisera le component suivant
+    component: MeteoComponent,
+  },
+  {
+    path: "meteo/:name", // la page affichant la météo prendra comme paramètre 'name'
+    component: MeteoDetailComponent, // Ce component fera l'appel AJAX et afficher les données reçues par openWeatherMap
+  },
+  {
+    path: "**", // un chemin vers une page inexistante redirigera vers '/'
+    redirectTo: "/",
+    pathMatch: "full",
+  },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     MeteoComponent,
+    MeteoDetailComponent
     // <-- dans la suite du TP,ajouter MeteoDetailComponent ici
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    CommonModule,
     ReactiveFormsModule,
     RouterModule,      
     AppRoutingModule
